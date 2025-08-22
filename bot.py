0685574197a3770edf920749bcac8ec98ed3bde5
kdavi <davidson_kevin_isa01@student.smc.edu>
2025-08-21 21:33:35 -0700

    fixed bugs

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 # set up the bot with the necessary intents
-intents - discord.Intents.default()
+intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
