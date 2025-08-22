d3c0a82d1a0dab69749cdc2076d589dee4b1a9cd
kdavi <davidson_kevin_isa01@student.smc.edu>
2025-08-21 21:35:21 -0700

    fixed another bug

--- bot.py
+++ bot.py
@@ -13,13 +13,14 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
+    """This function runs when the bot is connected and ready."""
     print(f'We have logged in as {client.user}')
-    print('Ready to go@')
-
-client.run(TOKEN)
+    print('Ready to go!')
 
 @client.event
 async def on_message(message):
+    """This function runs every time a message is sent."""
+    # Ignore messages sent by the bot itself to prevent loops
     if message.author == client.user:
         return
 
@@ -28,3 +29,6 @@ async def on_message(message):
 
     if message.content.startswith('!bye'):
         await message.channel.send('Goodbye! See you next time!')
+
+# This line MUST be the last thing to run
+client.run(TOKEN)
